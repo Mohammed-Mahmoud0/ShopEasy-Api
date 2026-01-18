341f2596da344af5ce29c1bc4380ff2722ba4888
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2026-01-18 04:57:30 +0200

    Add initial setup for store and tags apps, update settings and urls, and create product and customer models

---
+++ store/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateField(auto_now=False)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "Gold"),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
+    member_ship = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
+
