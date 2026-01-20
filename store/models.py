596bc31bec0a5ce5684be83fad291cfc93e1d8fb
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2026-01-20 15:22:20 +0200

    Add likes app with LikedItem model and related migrations; update product model to include slug and rename price to unit_price

--- store/models.py
+++ store/models.py
@@ -15,8 +15,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=7, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=False)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -68,6 +69,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
