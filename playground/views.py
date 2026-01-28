5107f339f9ffc026776870d683434b689aba83d6
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-01-28 16:36:30 +0200

    practice on select related & prefetch related to solve n+1 problems

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.db.models import Q
-from store.models import Product
+from django.db.models import Q, F
+from store.models import Product, OrderItem, Order
+
 # Create your views here.
 
 
 def say_hello(request):
 
-    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    queryset = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set__product")
+        .order_by("-placed_at")[:5]
+    )
 
     return render(request, "hello.html", {"name": "moh", "products": queryset})
