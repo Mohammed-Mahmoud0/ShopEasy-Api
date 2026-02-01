e29fefef5cee16d26c3ddb8a865086be3d0e4f74
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-02-01 23:51:33 +0200

    practice of using transactions

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Avg, Count, Max, Min, Sum
-from django.db.models import Value, F, Func
-from django.db.models.functions import Concat
-from store.models import Product, OrderItem, Order, Customer
+from django.db import transaction
+from store.models import Collection, Product, OrderItem, Order, Customer
 
 # Create your views here.
 
 
+# @transaction.atomic() # 1 this will wrap all the view with transaction
 def say_hello(request):
-    queryset = Customer.objects.annotate(num_orders=Count("order"))
 
-    return render(request, "hello.html", {"name": "moh", "result": queryset})
+    # 2 this give more control of which parts you want to be on transaction
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1  # type: ignore
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1  # type: ignore
+        # this not work cuz no id = -1 so all the operation will refused (thats the powerful of transactions)
+        # item.product_id = -1  # type: ignore
+        item.quantity = 1
+        item.unit_price = 10  # type: ignore
+        item.save()
+
+    return render(request, "hello.html", {"name": "moh"})
