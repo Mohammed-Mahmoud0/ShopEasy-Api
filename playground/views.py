38917937dc4f125933d3a70077785fc472902818
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-02-02 00:18:04 +0200

    practice writing raw sql

--- playground/views.py
+++ playground/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Collection, Product, OrderItem, Order, Customer
 
 # Create your views here.
 
 
-# @transaction.atomic() # 1 this will wrap all the view with transaction
 def say_hello(request):
+    # use cursor is good and give you more control of the all db 
+    # method 1 but should use try final and also need to close the cursor
+    cursor = connection.cursor()
+    cursor.execute("select * from store_product")
+    cursor.close()
+    
+    # method 2 is professional abd the cursor close automatically 
+    with connection.cursor() as cursor:
+        cursor.execute("select * from store_product")
+    
+    queryset = Product.objects.raw("select * from store_product")
 
-    # 2 this give more control of which parts you want to be on transaction
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1  # type: ignore
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1  # type: ignore
-        # this not work cuz no id = -1 so all the operation will refused (thats the powerful of transactions)
-        # item.product_id = -1  # type: ignore
-        item.quantity = 1
-        item.unit_price = 10  # type: ignore
-        item.save()
-
-    return render(request, "hello.html", {"name": "moh"})
+    return render(request, "hello.html", {"name": "moh", "result": list(queryset)})
