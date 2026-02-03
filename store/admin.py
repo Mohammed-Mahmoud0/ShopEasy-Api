db605b2d61f6fb415773698380f728d8878e8ce8
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-02-03 19:12:46 +0200

    Add store_custom app with custom Product admin and tagging functionality

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib import admin
 from django.db.models import Count
 from django.db.models.query import QuerySet
@@ -8,12 +9,29 @@ from django.utils.http import urlencode
 from django.urls import reverse
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [("<10", "Low")]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
     ordering = ["title"]
     list_select_related = ["collection"]
+    list_filter = ["collection", "last_update", InventoryFilter]
+    prepopulated_fields = {"slug": ["title"]}
+    autocomplete_fields = ["collection"]
+    search_fields = ["title"]
     list_per_page = 10
 
     def collection_title(self, product):
@@ -25,12 +43,22 @@ class ProductAdmin(admin.ModelAdmin):
             return "Low"
         return product.inventory
 
+    @admin.action(description="clear inventory")
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"{updated_count} products were successfully updated",
+        )
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
+    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+    search_fields = ["first_name", "last_name"]
     list_per_page = 10
 
     @admin.display(ordering="orders_count")
@@ -50,14 +78,23 @@ class CustomerAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(orders_count=Count("order"))
 
 
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    autocomplete_fields = ["product"]
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["id", "placed_at", "customer"]
+    autocomplete_fields = ["customer"]
+    inlines = [OrderItemInline]
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["title", "products_count"]
+    search_fields = ["title"]
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
