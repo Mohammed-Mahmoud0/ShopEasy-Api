e2e83adf9d33458329630ac94fe270c33103b776
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-03-03 15:11:33 +0200

    Add CartItem model, serializer, and viewset; update URLs for nested routing

--- store/views.py
+++ store/views.py
@@ -65,3 +65,12 @@ class CartViewSet(
 ):
     queryset = Cart.objects.prefetch_related("items__product").all()
     serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs["cart_pk"]).select_related(
+            "product"
+        )
