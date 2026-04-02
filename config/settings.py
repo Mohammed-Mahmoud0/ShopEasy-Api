5a36b753a58faaeb19edd07fcfac32c18baeff87
Mohammed-Mahmoud0 <mf.mohamedfawzy0@gmail.com>
2026-04-02 19:09:23 +0200

    add cors-headers for allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "corsheaders",
     "rest_framework",
     "djoser",
     "playground",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -53,6 +55,11 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
 ]
 
 ROOT_URLCONF = "config.urls"
